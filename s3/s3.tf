3033c3b4b0937a33db5bfb2e21f1afa07c87649a
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 20:58:10 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,9 +1,13 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-terra-bucket"
-  acl    = "private"
 
   tags = {
     Name        = "Jenkins S3 Bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
