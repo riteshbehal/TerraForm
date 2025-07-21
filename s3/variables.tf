63a0b6a21d535e668c9b85fdb02d0c3bbbb81291
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 20:24:14 +0530

    Update variables.tf

--- s3/variables.tf
+++ s3/variables.tf
@@ -2,9 +2,3 @@ variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
 }
-
-variable "aws_region" {
-  description = "The AWS region to deploy resources in"
-  type        = string
-  default     = "us-east-1"
-}
