51c47b0ce06f4bc84029056369b69abb6a9b4d4d
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 22:18:25 +0530

    Delete ec2/ec2.tf

--- ec2/ec2.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316"  # Replace with latest valid AMI for us-east-1
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "JenkinsDemoEC2"
-  }
-}
