2b5514b5ace8099d5fe0ff254a79713523afcd6b
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 18:01:29 +0530

    Update main.tf

--- s3/main.tf
+++ s3/main.tf
@@ -1,4 +1,13 @@
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+  bucket        = var.bucket_name
   force_destroy = true
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = "Dev"
+  }
 }
