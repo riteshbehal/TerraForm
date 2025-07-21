40319ca967bbeebb9d4a9eff269822c82a4ecaae
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 20:35:43 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,14 +1,7 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket        = var.bucket_name
-  force_destroy = true
+  bucket = var.bucket_name
 
   tags = {
-    Name        = var.bucket_name
-    Environment = "Dev"
+    CreatedBy = "Terraform via Jenkins"
   }
-}
-
-resource "aws_s3_bucket_acl" "bucket_acl" {
-  bucket = aws_s3_bucket.my_bucket.id
-  acl    = "private"
 }
