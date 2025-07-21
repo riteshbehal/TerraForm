ef1a97275f46559feeb055ef8949c5d4d22364c6
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 22:28:32 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,36 +1,23 @@
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-  default     = ""
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-  default     = ""
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
 }
 
-
 variable "ami" {
-   type        = string
-   description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+  description = "Ubuntu AMI ID"
+  type        = string
+  default     = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type" {
-   type        = string
-   description = "Instance type"
-   default     = "t2.micro"
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
 }
 
 variable "name_tag" {
-   type        = string
-   description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
+  description = "Name of the EC2 instance"
+  type        = string
+  default     = "My EC2 Instance"
 }
