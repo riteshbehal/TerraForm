be9d803514f2dbdc12be6940eab4cd8bb03c552d
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 19:43:18 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,9 +1,16 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "ritesh-jenkins-demo-bucket-001"
+  bucket = var.bucket_name
   acl    = "private"
 
   tags = {
     Name        = "Jenkins S3 Bucket"
     Environment = "Dev"
   }
+
+  force_destroy = true  # Important: allows deletion even if bucket is not empty
 }
