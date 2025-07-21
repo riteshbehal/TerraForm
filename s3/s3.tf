27f518267d7d309312f4fcf2a1158422b696bd90
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 21:59:50 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,8 +1,8 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  force_destroy = true
+resource "random_id" "bucket_suffix" {
+  byte_length = 4
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.my_bucket.id
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = "${var.bucket_name}-${random_id.bucket_suffix.hex}"
+  force_destroy = true
 }
