f7a495f8875b22cc0b6306707e62257be8fb322e
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 22:38:36 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,23 +1,35 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type        = string
+  default     = ""
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+  type        = string
+  default     = ""
+}
+
 variable "aws_region" {
   description = "AWS region"
   type        = string
-  default     = "ap-south-1"
+  default     = "us-east-1"
 }
 
 variable "ami" {
-  description = "Ubuntu AMI ID"
+  description = "Ubuntu AMI ID in us-east-1"
   type        = string
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-053b0d53c279acc90" # Ubuntu Server 22.04 LTS (HVM)
 }
 
 variable "instance_type" {
-  description = "Instance type"
+  description = "EC2 instance type"
   type        = string
   default     = "t2.micro"
 }
 
 variable "name_tag" {
-  description = "Name of the EC2 instance"
+  description = "Name tag for the EC2 instance"
   type        = string
   default     = "My EC2 Instance"
 }
