a3e22738835c8a99ecc2731cef70237bd7a4e0df
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 20:10:28 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,11 +1,18 @@
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
-  acl    = "private"
+provider "aws" {
+  region = "us-east-1"  #Updated to match your working region
+}
 
-  force_destroy = true  # Allows deleting non-empty buckets
+resource "aws_s3_bucket" "my_bucket" {
+  bucket        = "my-terra-bucket"
+  force_destroy = true
 
   tags = {
-    Name        = var.bucket_name
+    Name        = "my-terra-bucket"
     Environment = "Dev"
   }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.my_bucket.id
+  acl    = "private"
 }
