fef88956b9fca50dd9d5877c07bafa0b427f73f5
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 20:44:53 +0530

    Update s3.tf

--- s3/s3.tf
+++ s3/s3.tf
@@ -1,7 +1,9 @@
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucket_name
+  bucket = "my-terra-bucket"
+  acl    = "private"
 
   tags = {
-    CreatedBy = "Terraform via Jenkins"
+    Name        = "Jenkins S3 Bucket"
+    Environment = "Dev"
   }
 }
