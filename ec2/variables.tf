cb18349637ae960482f45546b85fa6bb74e45b0d
Ritesh Behal <49110318+riteshbehal@users.noreply.github.com>
2025-07-21 17:29:00 +0530

    Update variables.tf

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -1,9 +1,16 @@
-variable "name" {
-  description = "Name of the EC2 instance"
+variable "instance_name" {
+  description = "The EC2 instance name"
   type        = string
 }
 
-variable "region" {
-  description = "AWS Region"
+variable "ami_id" {
+  description = "The AMI to use for the instance"
   type        = string
+  default     = "ami-0c7217cdde317cfec"  # Amazon Linux 2 (us-east-1)
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+  default     = "t2.micro"
 }
